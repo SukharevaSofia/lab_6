c1b5cb3f4157b7535bfdeb0beeb17e5b0a02762e
sukha <334641@niuitmo.ru>
2022-05-29 03:43:00 +0300

    i am very disappointed in my life choices...

--- client/src/main/java/tools/CommandFinder.java
+++ client/src/main/java/tools/CommandFinder.java
@@ -25,32 +25,34 @@ public class CommandFinder {
 
         String[] userCommand = ConsoleUI.reader.nextLine().split(" ");
         String userCommandCheck = Arrays.stream(userCommand)
-                .collect(Collectors.joining(""));
+            .collect(Collectors.joining(""));
         List<String> commandsLen1 = List.of("help", "info", "show", "clear",
-                "print_field_descending_number_of_participants", "exit", "insert");
-        List<String> commandsLen2 = List.of("remove_key", "remove_greater","replace_if_lower",
-                "remove_greater_key","count_greater_than_description", "filter_less_than_genre");
+            "print_field_descending_number_of_participants", "exit", "insert");
+        List<String> commandsLen2 = List.of("remove_key", "remove_greater", "replace_if_lower",
+            "remove_greater_key", "count_greater_than_description", "filter_less_than_genre");
         if (userCommandCheck.equals(""))
           continue;
         if (userCommand.length > 2)
           throw new CommandException();
 
-        switch(userCommand.length){
+        switch (userCommand.length) {
 
-          case(1):{
-            if(commandsLen1.contains(userCommand[0])){
+          case (1): {
+            if (commandsLen1.contains(userCommand[0])) {
               return new CommandSerialize(userCommand[0]);
+            } else if (userCommand[0].equals("insert")) {
+              return new CommandSerialize(userCommand[0], "", dataInput.askMusicBand());
             }
           }
 
           case (2):
-            if(userCommand.length == 2){
-              if(commandsLen2.contains(userCommand[0])){
+            if (userCommand.length == 2) {
+              if (commandsLen2.contains(userCommand[0])) {
                 return new CommandSerialize(userCommand[0], userCommand[1]);
-              } else if(userCommand[0].equals("update")){
-                return new CommandSerialize(userCommand[0], userCommand[1], dataInput.askMusicBand());
-              } else if(userCommand[0].equals( "insert")){
-                return new CommandSerialize(userCommand[0], userCommand[1], dataInput.askMusicBand());
+              } else if (userCommand[0].equals("update")) {
+                return new CommandSerialize(userCommand[0], "band", dataInput.askMusicBand());
+              } else if (userCommand[0].equals("insert")) {
+                return new CommandSerialize(userCommand[0], "band", dataInput.askMusicBand());
               }
             }
 
